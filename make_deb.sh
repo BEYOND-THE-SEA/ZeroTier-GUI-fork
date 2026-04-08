d09f2e489621801ef10ca347b867e1f9ab1dbb25
TomJuzanx <tom.juzanx@beyond-the-sea.fr>
2026-04-08 12:17:40 +0200

    feat: enhance packaging and permissions management with ACL support

--- make_deb.sh
+++ make_deb.sh
@@ -11,6 +11,8 @@ mkdir -pv packaging/usr/share/licenses/ZeroTier-GUI
 mkdir -pv packaging/usr/share/pixmaps
 mkdir -pv packaging/usr/share/applications
 mkdir -pv packaging/usr/share/doc/ZeroTier-GUI
+mkdir -pv packaging/usr/share/polkit-1/actions
+mkdir -pv packaging/usr/libexec
 mkdir -pv packaging/usr/bin
 
 
@@ -21,6 +23,12 @@ cp -vf img/zerotier-gui.png packaging/usr/share/pixmaps
 cp -vf zerotier-gui.desktop packaging/usr/share/applications
 cp -vf README.md packaging/usr/share/doc/ZeroTier-GUI
 cp -vf src/zerotier-gui packaging/usr/bin
+cp -vf src/zerotier-gui-helper packaging/usr/libexec
+cp -vf linux/polkit/com.github.tralph3.zerotier-gui.policy packaging/usr/share/polkit-1/actions
+
+chmod 755 packaging/usr/bin/zerotier-gui
+chmod 755 packaging/usr/libexec/zerotier-gui-helper
+chmod 644 packaging/usr/share/polkit-1/actions/com.github.tralph3.zerotier-gui.policy
 
 
 # Create control file
@@ -29,15 +37,18 @@ echo "Package: zerotier-gui
 Version: ${packageVersion}
 Architecture: all
 Maintainer: tralph3
-Depends: python3-tk,policykit-1,python3 (>=3.6)
+Depends: python3-tk,policykit-1,python3 (>=3.6),acl,iproute2
 Priority: optional
 Homepage: https://github.com/tralph3/ZeroTier-GUI
 Description: A Linux front-end for ZeroTier" > packaging/DEBIAN/control
 
 # Build the package
 
-dpkg-deb --build packaging
+dpkg-deb --root-owner-group --build packaging
 
 # Rename the package
 
 mv packaging.deb ZeroTier-GUI.deb
+
+# Ensure predictable ownership for apt/_apt access
+chmod a+r ZeroTier-GUI.deb
